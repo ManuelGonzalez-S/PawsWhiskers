0c39fa5f783eb2b7b18c2e3760ddb9f3742a1c5c
Manuel Gonzalez <manuyako45@gmail.com>
2024-03-11 10:57:59 +0100

    Pendiente Firebase

---
+++ app/src/main/java/com/example/pawswhiskers/ui/informacion/InformacionFragment.kt
@@ -0,0 +1,36 @@
+package com.example.pawswhiskers.ui.informacion
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.pawswhiskers.databinding.FragmentInformacionBinding
+
+class InformacionFragment : Fragment() {
+
+    private var _binding: FragmentInformacionBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentInformacionBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/pawswhiskers/ui/tienda/TiendaFragment.kt
@@ -0,0 +1,36 @@
+package com.example.pawswhiskers.ui.tienda
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.pawswhiskers.databinding.FragmentTiendaBinding
+
+class TiendaFragment : Fragment() {
+
+    private var _binding: FragmentTiendaBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentTiendaBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
